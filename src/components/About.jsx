f621f8689f964500bd8b338deefd6b69f5f92f78
VIPIN <71195184+vipinkrm@users.noreply.github.com>
2022-05-10 10:38:51 +0530

    update links

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,10 +7,10 @@ const About = () => {
             <h2>Designed and Developed by:</h2>
             <h1>Vipin Kumar Maurya</h1>
             <div>
-                <a href="https://github.com/kvipinm" target='_blank' rel="noreferrer" className="about-icons">
+                <a href="https://github.com/vipinkrm" target='_blank' rel="noreferrer" className="about-icons">
                     <FaGithub size={70} />
                 </a>
-                <a href="https://www.linkedin.com/in/vipinkrmaurya/" target='_blank' rel="noreferrer" className="about-icons">
+                <a href="https://www.linkedin.com/in/vipinkrm/" target='_blank' rel="noreferrer" className="about-icons">
                     <FaLinkedin size={70} />
                 </a>
             </div>
